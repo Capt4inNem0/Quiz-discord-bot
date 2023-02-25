0201df4fe924a77a9d70cbad7b8169092efbe045
Capt4inNem0 <gastoncollo@gmail.com>
2023-02-25 01:14:28 -0300

    [REM] Dotenv dependency

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from dotenv import load_dotenv
-import os
-
 from DiscordBot import Bot
-
-load_dotenv()
 
 bot = Bot(command_prefix='?')
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = 'DISCORD_TOKEN'
 bot.run(TOKEN)
